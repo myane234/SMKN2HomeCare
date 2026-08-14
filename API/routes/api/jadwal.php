45110b2c17f7f70afc9d90203c2b308c9ae6da51
myane <157201609+myane234@users.noreply.github.com>
2026-08-14 19:00:23 +0700

    hapus API pindah repo baru

--- API/routes/api/jadwal.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\JadwalKerjaController;
-
-Route::middleware(['auth:sanctum'])->group(function () {
-    Route::get('/jadwal', [JadwalKerjaController::class, 'index']);
-    Route::post('/jadwal', [JadwalKerjaController::class, 'store']);
-});
