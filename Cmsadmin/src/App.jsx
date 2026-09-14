baa5a12a2a2a07c45df0b5690f2ace295b28f6f4
zhua1704 <sukaryanibudiarti@gmail.com>
2026-09-14 15:28:36 +0700

    Fix Profile admin

--- Cmsadmin/src/App.jsx
+++ Cmsadmin/src/App.jsx
@@ -69,15 +69,7 @@ function App() {
         element={<Navigate to="/dashboard" replace />}
       />
 
-      {/* Rute Profile di luar AdminLayout */}
-      <Route
-        path="/profile"
-        element={
-          <ProtectedRoute requiredPath="/profile">
-            <AdminProfile />
-          </ProtectedRoute>
-        }
-      />
+   
 
       {/* Main Admin/Super Admin App Layout */}
       <Route
@@ -369,6 +361,15 @@ function App() {
           element={
             <ProtectedRoute requiredPath="/laporan">
               <PageLaporan />
+            </ProtectedRoute>
+          }
+        />
+        {/* Pindahkan rute profile ke sini agar memiliki sidebar */}
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute requiredPath="/profile">
+              <AdminProfile />
             </ProtectedRoute>
           }
         />
